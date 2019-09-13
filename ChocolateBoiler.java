fe66a44833bb7d5486d23d625541d0b313b3cf5c
L4mb0 <juan.navarro@utec.edu.pe>
2019-09-13 11:34:06 -0500

    singleton done

--- ChocolateBoiler.java
+++ ChocolateBoiler.java
@@ -2,6 +2,25 @@ public class ChocolateBoiler {
     private boolean empty;
     private boolean boiled;
 
+    private static ChocolateBoiler instance;
+    private static Object mutex = new Object();
+
+    public static ChocolateBoiler getInstance() {
+        ChocolateBoiler result = instance;
+        if (result == null) { //if null, create unique instance of chocolate boiler
+            synchronized (mutex) {
+                result = instance;
+                if (result == null) {
+                    instance = result = new ChocolateBoiler();
+                    System.out.println("thread enviado");
+                }
+            }
+        } else {
+            System.out.println("thread regresado");
+        }
+        return result;
+    }
+
     public ChocolateBoiler() {
         empty = true;
         boiled = false;
