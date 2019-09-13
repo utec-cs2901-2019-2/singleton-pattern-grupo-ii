20b900a753d3011442f5022fd4a603202d1486a1
GioCS17 <39771399+GioCS17@users.noreply.github.com>
2019-09-13 11:54:52 -0500

    Delete main.java

--- main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-	public static void main(String[] args){ 
-        int n = 8; // Number of threads 
-        for (int i=0; i<8; i++){ 
-            Thread singletonThreads = new Thread(new Multithread()); 
-            singletonThreads.start(); 
-        } 
-    } 
-}
